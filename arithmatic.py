ae34f7dec1a70ebebcea395e5a67025c46d8a995
Jannatul Ferdous Eva <jannatuleva990@gmail.com>
2021-03-17 14:26:26 +0600

    first python code

---
+++ arithmatic.py
@@ -0,0 +1,10 @@
+# Arithmatic option
+
+a = print("sum = ",10+3)
+b = print('subtraction =',10-3)
+c = print('Multiplication =',10*3)
+d = print('Division =',10/3)
+e = print('Double Division = ',10//3)
+f = print("Remainder = ",10%3)
+g = print("Power = ",10**3)
+
